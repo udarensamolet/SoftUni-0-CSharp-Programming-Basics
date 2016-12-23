593ce1c1518d00cce43b46a9048c29d066fbfcfc
Rositsa Nenova <udarensamolet@yahoo.com>
2016-12-23 21:30:07 +0200

    Add files via upload

---
+++ 06. DrawingWithLoops/Butterfly/Butterfly.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Butterfly
+{
+    class Butterfly
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //top
+            for (int row = 0; row < n - 2; row++)
+            {
+                if (row % 2 != 0)
+                {
+                    Console.Write(new string('-', n - 2));
+                    Console.Write("\\ /");
+                    Console.Write(new string('-', n - 2));
+                }
+                else
+                {
+                    Console.Write(new string('*', n - 2));
+                    Console.Write("\\ /");
+                    Console.Write(new string('*', n - 2));
+                }
+                Console.WriteLine();
+            }
+
+            //middle
+            Console.Write(new string(' ', n - 1));
+            Console.Write("@");
+            Console.WriteLine();
+
+            //bottom
+            for (int row = 0; row < n - 2; row++)
+            {
+                if (row % 2 != 0)
+                {
+                    Console.Write(new string('-', n - 2));
+                    Console.Write("/ \\");
+                    Console.Write(new string('-', n - 2));
+                }
+                else
+                {
+                    Console.Write(new string('*', n - 2));
+                    Console.Write("/ \\");
+                    Console.Write(new string('*', n - 2));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 06. DrawingWithLoops/ChristmasTree/ChristmasTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    class ChristmasTree
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //top
+            Console.Write(new string(' ', n+1));
+            Console.WriteLine("|");
+
+            //the other part
+            for (int row = 1; row <= n; row++)
+            {
+                Console.Write(new string(' ', n-row));
+                Console.Write(new string('*', row));
+                Console.Write(" | ");
+                Console.Write(new string('*', row));
+                Console.WriteLine();
+            }
+        }
+    }
+}
