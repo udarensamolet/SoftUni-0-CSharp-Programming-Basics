4ce39c603bfbdcdcc669a22fa9a1d1dd26940de2
Rositsa Nenova <udarensamolet@yahoo.com>
2016-12-23 21:32:04 +0200

    Add files via upload

---
+++ 06. DrawingWithLoops/RectangleOfNxNStars/RectangleOfNxNStars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RectangleOfNxNStars
+{
+    class RectangleOfNxNStars
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', n));
+            }
+        }
+    }
+}
