1cbf669f63f72897b0ca66d382885445e97a6b98
Rositsa Nenova <udarensamolet@yahoo.com>
2016-12-23 21:17:50 +0200

    Add files via upload

---
+++ 01. IntroductionToProgramming/Triangle55Stars/Triangle55Stars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Triangle55Stars
+{
+    class Triangle55Stars
+    {
+        static void Main()
+        {
+            for (int row = 1; row < 11; row++)
+            {
+                for (int col = 1; col < row + 1; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
