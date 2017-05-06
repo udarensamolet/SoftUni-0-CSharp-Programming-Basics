a85c8daa6ee529b75221422af8ac02b5fb395d88
Rositsa Nenova <udarensamolet@yahoo.com>
2017-05-06 23:59:59 +0300

    Add files via upload

---
+++ 09. Exams/2017March18/CarToGo/CarToGo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CarToGo
+{
+    class CarToGo
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine().ToLower();
+
+            double price = 0;
+            string carType = null;
+            string carClass = null;
+
+            if (budget <= 100)
+            {
+                carClass = "Economy class";
+                if (season == "summer")
+                {
+                    carType = "Cabrio";
+                    price = budget * 35 / 100;
+                }
+                else if (season == "winter")
+                {
+                    carType = "Jeep";
+                    price = budget * 65 / 100;
+                }
+            }
+
+            else if (budget > 100 && budget <= 500)
+            {
+                carClass = "Compact class";
+                if (season == "summer")
+                {
+                    carType = "Cabrio";
+                    price = budget * 45 / 100;
+                }
+                else if (season == "winter")
+                {
+                    carType = "Jeep";
+                    price = budget * 80 / 100;
+                }
+            }
+
+            else if (budget > 500)
+            {
+                carClass = "Luxury class";
+                carType = "Jeep";
+                price = budget * 90 / 100;
+            }
+
+            Console.WriteLine("{0}", carClass);
+            Console.WriteLine("{0} - {1:f2}", carType, price);
+        }
+    }
+}
---
+++ 09. Exams/2017March18/SumOfTwoNumbers/SumOfTwoNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class SumOfTwoNumbers
+    {
+        static void Main()
+        {
+            double start = double.Parse(Console.ReadLine());
+            double end = double.Parse(Console.ReadLine());
+            double magicNumber = double.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (double i = start; i <= end; i++)
+            {
+                for (double j = start; j <= end; j++)
+                {
+                    counter++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine("Combination N:{0} ({1} + {2} = {3})", counter, i, j, magicNumber);
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("{0} combinations - neither equals {1}", counter, magicNumber);
+        }
+    }
+}
